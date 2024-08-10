8de5b308f989a5e3ff3abf2a781153c489df37ca
suzanna8767 <codessuzy@gmail.com>
2024-08-09 21:40:23 -0400

    I have created the create recipe method!

---
+++ script/index.js
@@ -0,0 +1,66 @@
+window.onload = onLoadFn;
+
+function onLoadFn() {
+    let recipeSection = document.getElementById("recipeSection");
+    let recipeTitleElem = document.getElementById("recipeTitle");
+    let waterMLElem = document.getElementById("waterML");
+    let pourTimeElem = document.getElementById("pourTime");
+    let clearDataBtn = document.getElementById("clearData");
+
+    // If there are no recipes, display the no recipe message
+    // If there are recipes, then call the recipe display function
+    if (localStorage.getItem("espressoRecipeList") === null) {
+        $("#recipeSection").hide()
+      } else {
+        $("#noneSection").hide()
+        $("#recipeSection").show()
+        getRecipe();
+        console.log("I am loading recipe");
+      }
+    
+    // Function that displays the recipes
+    function getRecipe(){
+
+        //Getting the recipes 
+        let recipes = JSON.parse(localStorage.getItem("espressoRecipeList"));
+        
+        // Clear the section first
+        recipeSection.innerHTML = '';
+
+        // For loop that will display all the recipes stored in the recipe array 
+        recipes.forEach((recipe, index) => {
+            let recipeElem = document.createElement("div");
+            recipeElem.className = "recipeList";
+            recipeElem.innerHTML = `
+                <h2>${recipe.coffeeBean}</h2> 
+                    <div class="listRight">
+                        <p>${recipe.yieldML} ML</p>
+                        <p>${recipe.brewingTime} Seconds</p>
+                        <a href="addRecipe.html"><p class="brewNowBtn">Brew Now</p></a>
+                    </div>
+            `;
+
+            recipeSection.appendChild(recipeElem); 
+        });
+
+        console.log ("Recipes all displayed")
+
+    }
+
+    function clearData(){
+        localStorage.removeItem("espressoRecipeList"); // Corrected this line
+        $("#recipeSection").hide();
+        $("#noneSection").show();
+        console.log("Recipe data cleared");
+    }
+
+    clearDataBtn.onclick = clearData;
+}  
+        // let recipeTitle = espressoRecipe.coffeeBean;
+        // let waterML = espressoRecipe.yieldML;
+        // let pourTime = espressoRecipe.brewingTime;
+        
+        // recipeTitleElem.innerHTML = recipeTitle; 
+        // waterMLElem.innerHTML = waterML + "ML"; 
+        // pourTimeElem.innerHTML = pourTime + "Seconds"; 
+        // console.log("I am loading recipe in function");
---
+++ script/recipe.js
@@ -0,0 +1,55 @@
+window.onload = onLoadFn;
+
+
+function onLoadFn() {
+
+    //Form Element Declare
+    let addRecipeForm = document.getElementById("addEspressoRecipe");
+    let coffeeBeanElem = document.getElementById("coffeeBean");
+    let beanGramsElem = document.getElementById("beanGrams");
+    let yieldMLElem = document.getElementById("yieldML");
+    let brewingTimeElem = document.getElementById("brewingTime");
+    let temperatureElem = document.getElementById("temperature");
+
+    function onSubmit(event){
+        
+        // Capture form data!
+        let coffeeBean = coffeeBeanElem.value;
+        let beanGrams = beanGramsElem.value;
+        let yieldML = yieldMLElem.value;
+        let brewingTime = brewingTimeElem.value;
+        let temperature = temperatureElem.value;
+
+       
+        //Create object for espressoRecipe
+        const espressoRecipe = {
+            coffeeBean: coffeeBean,
+            beanGrams: beanGrams,
+            yieldML: yieldML,
+            brewingTime: brewingTime,
+            temperature: temperature
+        };
+
+        // Array for a list of recipes
+        let recipeList = [];
+        
+        // Let's check if the recipe list is empty or not.
+        // If it is not null, then let's load the old recipe list first. 
+        if (localStorage.getItem("espressoRecipeList") !== null) {
+            recipeList = JSON.parse(localStorage.getItem("espressoRecipeList"));
+        } 
+
+        // Pushing the new recipe into the recipe list array 
+        recipeList.push(espressoRecipe);
+       
+        localStorage.setItem("espressoRecipeList", JSON.stringify(recipeList));
+
+        console.log ("New Recipe Added");
+        window.location.href = "../views/index.html";
+        event.preventDefault();
+    }
+    
+    addRecipeForm.onsubmit = onSubmit;
+
+
+}
